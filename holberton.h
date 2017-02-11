74a1f4cdaa5fa439b1530ab467c82bc332d9d37d
Rick-Houser <frakentoaster@gmail.com>
2017-02-11 15:23:09 -0800

    add header file containing print_char function

---
+++ holberton.h
@@ -0,0 +1,7 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+/* Function that produces output according to a format */
+int _printf(const char *format, ...);
+/* print_char - Function that prints out a given character */
+int print_char(char c);
+#endif
