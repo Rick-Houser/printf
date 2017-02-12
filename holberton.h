38eeade592d1d56cd1e76eb96aad8effe6c89f6e
FrackenToaster <frakentoaster@gmail.com>
2017-02-12 12:14:33 -0800

    update header with new prototype that checks for consecutive '%' and returns a count

--- holberton.h
+++ holberton.h
@@ -6,4 +6,6 @@ int _printf(const char *format, ...);
 int print_char(char c);
 /* print_char - Function that prints out a string of characters */
 void print_string(char *t);
+/* print_p - Function that checks for consecutive '%' and returns count. */
+int print_p(char c, int count);
 #endif
