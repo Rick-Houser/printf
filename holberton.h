4107611ba1aba8aa9efe9720e68d75cdf495a4b1
Rick-Houser <frakentoaster@gmail.com>
2017-02-11 16:43:40 -0800

    update header with print_string prototype

--- holberton.h
+++ holberton.h
@@ -4,4 +4,6 @@
 int _printf(const char *format, ...);
 /* print_char - Function that prints out a given character */
 int print_char(char c);
+/* print_char - Function that prints out a string of characters */
+void print_string(char *t);
 #endif
