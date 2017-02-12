6bb7f45d9b19d5046d3eb54225a548dca50091fe
FrackenToaster <frakentoaster@gmail.com>
2017-02-11 16:43:14 -0800

    update _printf to pass Betty checks

--- _printf.c
+++ _printf.c
@@ -1,44 +1,41 @@
 #include<stdio.h>
 #include<stdarg.h>
 #include "holberton.h"
+/**
+ * _printf - function that produces output according to a format.
+ * @format: character string composed of 0 or more directives.
+ *
+ * Return: the number of characters printed(excluding null at end of strings).
+ */
 
-int _printf(const char *format,...)
+int _printf(const char *format, ...)
 {
-	int i;
-	int c;
-	int count;
+	int i, c, count;
 	char *s;
-	int k;
-
 	va_list arg;
+
 	va_start(arg, format);
 	i = 0;
 	count = 0;
+
 	while (format[i] != '\0')
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			switch(format[i])
+			switch (format[i])
 			{
-				case 'c' :
+				case 'c':
 					c = va_arg(arg, int);
 					print_char(c);
 					break;
 
 				case 's':
 					s = va_arg(arg, char *);
-					for (k = 0; s[k]; k++)
-					{
-						print_char(s[k]);
-					}
+					print_string(s);
 					break;
 
 				case '%':
-					if (format[i+1] == '%')
-					{
-						count--;
-					}
 					print_char('%');
 					break;
 			}
