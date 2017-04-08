c52f56baaae510ceae96b5d49dd54db63bcf778c
Rick-Houser <frakentoaster@gmail.com>
2017-04-08 12:32:03 -0700

    Add comment for return statement

--- print_char.c
+++ print_char.c
@@ -3,7 +3,7 @@
  * print_char - Function that prints out a given character.
  * @c: A character to be printed.
  *
- * Return: The ...
+ * Return: The number of characters written.
  */
 
 int print_char(char c)
