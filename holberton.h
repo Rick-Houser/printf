5b46384d307615f1daaaf684c3945d58082da8c7
Rick-Houser <frakentoaster@gmail.com>
2017-04-08 12:31:32 -0700

    update header to reflect renamed functions

--- holberton.h
+++ holberton.h
@@ -4,12 +4,12 @@
 int _printf(const char *format, ...);
 /* print_char - Function that prints out a given character */
 int print_char(char c);
-/* print_char - Function that prints out a string of characters */
-void print_string(char *t);
+/* print_char - Function that prints out a string of chars and return count */
+int print_string(char *str);
 /* print_p - Function that checks for consecutive '%' and returns count. */
 int print_p(char c, int count);
 /* print_number - Function uses ascii to print each digit of int n */
-void print_number(int n);
+int print_number(int n);
 /* int_min - Helper function that handles minimum int value */
 void int_min(void);
 /* my_pow - Helper function that calculates a given power */
