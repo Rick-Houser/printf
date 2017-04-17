0c10eb6188a5666c8845c3143ad40eedbb5db79d
Rick-Houser <frakentoaster@gmail.com>
2017-04-16 20:27:28 -0700

    remove old function declaration

--- holberton.h
+++ holberton.h
@@ -6,8 +6,6 @@ int _printf(const char *format, ...);
 int print_char(char c);
 /* print_char - Function that prints out a string of chars and return count */
 int print_string(char *str);
-/* print_p - Function that checks for consecutive '%' and returns count. */
-int print_p(char c, int count);
 /* print_number - Function uses ascii to print each digit of int n */
 int print_number(int n);
 
